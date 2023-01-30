f108ee2e486aff9383bfc2c2cdf0595cdec07a51
Hardik Agarwal <agarwalhardik562@gmail.com>
2023-01-31 01:20:43 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import "./Components/css/App.css";
+import "./Components/css/MainApp.css";
 import Header from "./Components/NoteComponents/Header";
 import Notes from "./Components/NoteComponents/Notes";
 function App() {
